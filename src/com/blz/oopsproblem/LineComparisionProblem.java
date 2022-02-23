8a0cc007054c932cae72487180bbd81ca8f2fcbd
first_name <ss329222@gmail.com>
2022-02-23 07:21:36 +0530

    Added:EqualityOfLines

--- src/com/blz/oopsproblem/LineComparisionProblem.java
+++ src/com/blz/oopsproblem/LineComparisionProblem.java
@@ -4,28 +4,45 @@ import java.util.Scanner;
 
 public class LineComparisionProblem {
 
-	public static double lineValues() {
-		try (Scanner sc = new Scanner(System.in)) {
-			double x1, x2, y1, y2;
-
-			System.out.println("Enter Values For First Point");
-			x1 = sc.nextDouble();
-			y1 = sc.nextDouble();
-
-			System.out.println("Enter Values For Second Point");
-			x2 = sc.nextDouble();
-			y2 = sc.nextDouble();
-
-			double lineLength = (double) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-			return lineLength;
+	public class LineComparison {
+		public static double lineValues() {
+			 Scanner sc = new Scanner(System.in);
+				double x1,x2,y1,y2;
+				
+					System.out.println("Enter Values For First Point");
+					x1 = sc.nextDouble();
+					y1 = sc.nextDouble();
+				
+					System.out.println("Enter Values For Second Point");
+					x2 = sc.nextDouble();
+					y2 = sc.nextDouble();
+				
+					double lineLength = (double) Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+					return lineLength;
+			 	}	
+			
+			public static void checkEquality(double lineOneLength , double lineTwoLength ) {
+				if(lineOneLength == lineTwoLength ) {
+					System.out.println("Lines are Equal");
+			}
+				else {
+					System.out.println("Lines are not Equal");
+			}
 		}
+
+		
+		
+		
+		public static void main(String[] args) {
+			System.out.println("Welcome to Line Comparison Computation ");
+			
+			double lineOneLength = lineValues();
+				System.out.println("Length Of Line1 is: " + lineOneLength);
+			
+			double lineTwoLength = lineValues();
+				System.out.println("Length Of Line2 is: " + lineTwoLength);
+			
+				checkEquality(lineOneLength , lineTwoLength);
+		}
+
 	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line  Comparison Computation ");
-		double lineLength = lineValues();
-		System.out.println("Length Of Line is: " + lineLength);
-
-	}
-
-}
